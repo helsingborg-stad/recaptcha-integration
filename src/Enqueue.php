8486955ce865993d2b00f61c57e29f40d8bca905
Johan Silvergrund <johan.silvergrund@hiq.se>
2021-03-19 13:38:26 +0100

    Removed unnecessary method

--- src/Enqueue.php
+++ src/Enqueue.php
@@ -11,10 +11,14 @@ class Enqueue extends RecaptchaIntegration
     /**
      * Enqueue constructor.
      */
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
+    /**
+     * Enqueue Google reCAPTCHA
+     */
     /**
      * Enqueue Google reCAPTCHA
      */
@@ -23,20 +27,33 @@ class Enqueue extends RecaptchaIntegration
         if (defined('G_RECAPTCHA_KEY') && defined('G_RECAPTCHA_SECRET')) {
 
             wp_enqueue_script('municipio-google-recaptcha',
-                'https://www.google.com/recaptcha/api.js?render=' . G_RECAPTCHA_KEY);
+                'https://www.google.com/recaptcha/api.js?render=' . G_RECAPTCHA_KEY, 20, 'dev', true
+            );
 
             wp_add_inline_script('municipio-google-recaptcha', "
             
-                  var interval = setInterval(function(){
-                  if(window.grecaptcha){
-                    grecaptcha.ready(function() {
-                        grecaptcha.execute('" . G_RECAPTCHA_KEY . "', {action: 'homepage'}).then(function(token) {
-                            document.querySelector('.g-recaptcha-response').value = token;
+                function validateClient(e, formId) {
+                    e.preventDefault();
+                    
+                    if (window.grecaptcha) {
+                        grecaptcha.ready(function () {
+                            grecaptcha.execute('" . G_RECAPTCHA_KEY . "', {action: 'submit'}).then(function (token) {
+                                document.getElementById(formId).querySelector('.g-recaptcha-response').value = token;
+                                document.forms[formId].submit();
+                            });
                         });
-                    });
-                    clearInterval(interval);
-                  }
-                }, 100);
+                    }
+                }
+            
+                var checkFormAttributes = document.querySelectorAll('form');
+                for (var i = 0; i < document.getElementsByTagName('form').length; i++) {
+                    var formId = checkFormAttributes[i].getAttribute('id');
+                    if (document.getElementById(formId).getAttribute('method').toLowerCase() === 'post') {
+                        if (document.getElementById(formId).getElementsByClassName('protectedByCaptcha')[0]){
+                            document.getElementById(formId).setAttribute('onsubmit', 'validateClient(event, formId);');
+                        }
+                    }
+                }
             ");
         }
     }
